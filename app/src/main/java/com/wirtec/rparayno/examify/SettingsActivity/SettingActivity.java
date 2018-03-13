2747f8be448c74b1d29926e99773b0d596024c48
Richard Parayno <broodheart@gmail.com>
2018-03-13 17:15:53 +0800

    updated listview

--- app/src/main/java/com/wirtec/rparayno/examify/SettingsActivity/SettingActivity.java
+++ app/src/main/java/com/wirtec/rparayno/examify/SettingsActivity/SettingActivity.java
@@ -4,16 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.wirtec.rparayno.examify.R;
 
 public class SettingActivity extends AppCompatActivity {
+    private String[] settingsArray = {"Logout"};
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // remove title
         removeTitleBar();
+        initResources();
 
     }
 
@@ -22,5 +28,13 @@ public class SettingActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_setting);
+    }
+
+    private void initResources() {
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.listview_setting, settingsArray);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }
