0c0740ef325705643e9b18761e238ac295facb0b
Richard Parayno <broodheart@gmail.com>
2018-03-13 10:01:43 +0800

    started implementing profilecard2. added color to icons in bottom navbar when selected

---
+++ app/src/main/java/com/wirtec/rparayno/examify/ClassActivity/ClassActivity.java
@@ -0,0 +1,15 @@
+package com.wirtec.rparayno.examify.ClassActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wirtec.rparayno.examify.R;
+
+public class ClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class);
+    }
+}
